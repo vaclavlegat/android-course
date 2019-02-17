c3221e804595310b468f044f9e5ad000eb53b2b1
Václav Legát <vaclav.legat@profinit.eu>
2019-02-17 16:04:12 +0100

    Request contacts permission.

--- app/src/main/java/eu/profinit/profis/ui/UtilizationDetailActivity.java
+++ app/src/main/java/eu/profinit/profis/ui/UtilizationDetailActivity.java
@@ -1,10 +1,14 @@
 package eu.profinit.profis.ui;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,12 +20,15 @@ import java.util.Locale;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import eu.profinit.profis.R;
 import eu.profinit.profis.db.ProfisDatabase;
 import eu.profinit.profis.model.UtilizationItem;
 
 public class UtilizationDetailActivity extends AppCompatActivity {
 
+    private static final int CONTACTS_REQUEST = 1;
     private static final String ID_KEY = "ID_KEY";
 
     private TextView contract;
@@ -58,6 +65,8 @@ public class UtilizationDetailActivity extends AppCompatActivity {
 
 
         new LoadItemTask().execute(getIntent().getLongExtra(ID_KEY, 0));
+
+        checkPermission();
 
     }
 
@@ -130,5 +139,67 @@ public class UtilizationDetailActivity extends AppCompatActivity {
         dialog.show();
     }
 
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                CONTACTS_REQUEST);
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                // show explanation
+                new AlertDialog.Builder(this).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermission();
+                    }
+                }).setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setTitle(R.string.permission_contacts_title).setMessage(R.string.permission_contacts_message).create().show();
+
+            } else {
+                //request permission
+                requestPermission();
+            }
+        } else {
+            // we have permission
+            showContacts();
+        }
+    }
+
+    private void showContacts() {
+        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME},
+                ContactsContract.Contacts.DISPLAY_NAME + " like ?",
+                new String[]{"A%"},
+                null);
+
+        int contactsSize = 0;
+        while (cursor != null && cursor.moveToNext()) {
+            contactsSize++;
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        Toast.makeText(this, "Contacts size " + contactsSize, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (CONTACTS_REQUEST == requestCode && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            showContacts();
+        } else {
+            // permission denied
+        }
+    }
 
 }
