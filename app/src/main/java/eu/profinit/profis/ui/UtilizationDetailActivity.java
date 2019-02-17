9d446bb4663cfc87114c8b029e2f91a2222d6874
Václav Legát <vaclav.legat@profinit.eu>
2019-02-17 15:36:00 +0100

    Deleting detail from db.

--- app/src/main/java/eu/profinit/profis/ui/UtilizationDetailActivity.java
+++ app/src/main/java/eu/profinit/profis/ui/UtilizationDetailActivity.java
@@ -1,15 +1,20 @@
 package eu.profinit.profis.ui;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import eu.profinit.profis.R;
 import eu.profinit.profis.db.ProfisDatabase;
@@ -23,6 +28,9 @@ public class UtilizationDetailActivity extends AppCompatActivity {
     private TextView hours;
     private TextView date;
     private TextView note;
+    private Button deleteBtn;
+
+    private UtilizationItem selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,15 @@ public class UtilizationDetailActivity extends AppCompatActivity {
         hours = findViewById(R.id.hours);
         date = findViewById(R.id.date);
         note = findViewById(R.id.note);
+        deleteBtn = findViewById(R.id.delete_btn);
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDeleteDialog();
+            }
+        });
+
 
         new LoadItemTask().execute(getIntent().getLongExtra(ID_KEY, 0));
 
@@ -69,7 +86,48 @@ public class UtilizationDetailActivity extends AppCompatActivity {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd. MM. yyyy", Locale.getDefault());
             date.setText(dateFormat.format(item.getDate()));
             note.setText(item.getNote());
+            selectedItem = item;
         }
+    }
+
+    private void showSuccess() {
+        Toast.makeText(this, R.string.deleted, Toast.LENGTH_SHORT).show();
+    }
+
+    private void deleteItem(UtilizationItem item) {
+        ProfisDatabase.getInstance(this).utilizationDao().delete(item);
+    }
+
+    private class DeleteItemTask extends AsyncTask<UtilizationItem, Void, Void> {
+
+        @Override
+        protected Void doInBackground(UtilizationItem... items) {
+            deleteItem(items[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            showSuccess();
+            finish();
+        }
+    }
+
+    private void showDeleteDialog() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.delete_utilization_title))
+                .setMessage(getString(R.string.delete_utilization_message))
+                .setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        new DeleteItemTask().execute(selectedItem);
+                    }
+                }).setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                }).create();
+
+        dialog.show();
     }
 
 
